bc9b124c66802e0837f7f9efa4f6e9188da08023
VladaLukovskaya <49092946+VladaLukovskaya@users.noreply.github.com>
2019-04-16 10:17:41 +0300

    delete main.py

--- lesson3_simple_basic_functions/main.py
+++
@@ -1,8 +0,0 @@
-Sveta = input('Что делает Света? ')
-if 'готов' in Sveta:
-    Vlad = 'доволен'
-elif 'ест' or 'куш' in Sveta:
-    Vlad = 'счастлив'
-else:
-    Vlad = 'не доволен'
-print('Влад', Vlad)
