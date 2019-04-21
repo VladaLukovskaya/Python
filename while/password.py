2ebd3aa6a1ad0bf22d84da91cc8039f3cbf23549
VladaLukovskaya <49092946+VladaLukovskaya@users.noreply.github.com>
2019-04-21 18:25:46 +0300

    Update password.py

--- while/password.py
+++ while/password.py
@@ -1,11 +1,26 @@
 password = input('Enter your password: ')
 check = input('Enter the password again: ')
+correct = ''
 
-if len(password) < 8:
-    print('The password is short.')
-elif '123' in password:
-    print('The password is simple.')
-elif password != check:
-    print('These passwords are different.')
-else:
-    print('OK.')
+while correct != 'OK':
+    if len(password) < 8:
+        print('The password is short.')
+        new_password = input('Enter new password: ')
+        password = new_password
+        new_check = input('Enter new password again: ')
+        check = new_check
+    elif '123' in password:
+        print('The password is simple.')
+        new_password = input('Enter new password: ')
+        password = new_password
+        new_check = input('Enter new password again: ')
+        check = new_check
+    elif password != check:
+        print('These passwords are different.')
+        new_password = input('Enter new password: ')
+        password = new_password
+        new_check = input('Enter new password again: ')
+        check = new_check
+    else:
+        correct = 'OK'
+        print(correct)
